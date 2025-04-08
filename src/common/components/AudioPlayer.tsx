1a8b9ab6cb1a3ff871fdf5187c582bb9da3aac6f
Dyrudis <58176532+Dyrudis@users.noreply.github.com>
2025-04-08 19:44:13 +0200

    feat: add volume slider

--- src/common/components/AudioPlayer.tsx
+++ src/common/components/AudioPlayer.tsx
@@ -1,8 +1,9 @@
-import { HTMLAttributes, useCallback, useRef } from 'react'
-import { ArrowCounterClockwise, Pause, Play } from '@phosphor-icons/react'
+import { HTMLAttributes, useCallback, useEffect, useRef, useState } from 'react'
+import { ArrowCounterClockwise, Pause, Play, SpeakerHigh, SpeakerLow, SpeakerX } from '@phosphor-icons/react'
 import { useWavesurfer } from '@wavesurfer/react'
 
 import Button from '@components/Button'
+import Slider from '@components/Slider'
 
 type AudioPlayerProps = HTMLAttributes<HTMLDivElement> & {
   url: string
@@ -10,6 +11,8 @@ type AudioPlayerProps = HTMLAttributes<HTMLDivElement> & {
 
 function AudioPlayer({ url }: AudioPlayerProps) {
   const containerRef = useRef(null)
+  const [volume, setVolume] = useState(0.375)
+  const [isMuted, setIsMuted] = useState(false)
 
   const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
     container: containerRef,
@@ -27,28 +30,55 @@ function AudioPlayer({ url }: AudioPlayerProps) {
   })
 
   const handlePlayPause = useCallback(() => {
-    if (!wavesurfer) return
-    wavesurfer.playPause()
+    wavesurfer?.playPause()
   }, [wavesurfer])
 
   const handleRestart = useCallback(() => {
-    if (!wavesurfer) return
-    wavesurfer.stop()
-    wavesurfer.play()
+    wavesurfer?.stop()
+    wavesurfer?.play()
   }, [wavesurfer])
+
+  useEffect(() => {
+    wavesurfer?.setVolume(volume)
+    wavesurfer?.setMuted(isMuted)
+  }, [wavesurfer, volume, isMuted])
+
+  const toggleMuted = () => {
+    setIsMuted((prev) => !prev)
+  }
 
   return (
     <div className="w-full mb-4">
       <div className="mb-4 w-full max-w-md mx-auto px-4" ref={containerRef}></div>
       <div className="flex items-center justify-center gap-4">
-        <span className="w-12 text-center">{`${currentTime.toFixed(1)}s`}</span>
+        <span className="w-12 text-right font-mono font-extrabold">{`${currentTime.toFixed(1)}s`}</span>
         <Button onClick={handlePlayPause}>{isPlaying ? <Pause /> : <Play />}</Button>
         <Button onClick={handleRestart}>
           <ArrowCounterClockwise />
         </Button>
+        <div className="flex items-center gap-2">
+          <div onClick={toggleMuted} className="cursor-pointer">
+            <SpeakerIcon volume={volume} isMuted={isMuted} size={20} />
+          </div>
+          <Slider value={volume} min={0} max={.75} step={0.01} onChange={setVolume} className="w-32" />
+        </div>
       </div>
     </div>
   )
+}
+
+const SpeakerIcon = ({
+  volume = 0,
+  isMuted = false,
+  size = 16,
+}: {
+  volume?: number
+  isMuted?: boolean
+  size?: number
+}) => {
+  if (volume === 0 || isMuted) return <SpeakerX size={size} weight="fill" />
+  if (volume > 0 && volume <= 0.375) return <SpeakerLow size={size} weight="fill" />
+  if (volume > 0.375) return <SpeakerHigh size={size} weight="fill" />
 }
 
 export default AudioPlayer
